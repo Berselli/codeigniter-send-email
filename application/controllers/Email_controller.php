2c7eda378038f2a44b3fd0b915da2cde45b95f6f
Juliano Berselli <bersellib@gmail.com>
2018-12-04 08:01:20 -0200

    Multiplas alterações no controller Email_controller: * carregados os helpers de url e form para criação da view form_send_email; * criado o método send_email e configurado-o para no envio usar o protocolo smtp;

--- application/controllers/Email_controller.php
+++ application/controllers/Email_controller.php
@@ -20,6 +20,32 @@ class Email_controller extends CI_Controller {
 	 */
 	public function index()
 	{
+	    $this->load->helper(array('url', 'form'));
 		$this->load->view('form_send_email');
 	}
+	public function send_email(){
+	    $config = [
+            'protocol' => 'smtp',
+            'smtp_host' => 'smtp.mailtrap.io',
+            'smtp_port' => 2525,
+            'smtp_user' => 'c7c9ee20513111',
+            'smtp_pass' => '66810a97ea201a',
+            'crlf' => "\r\n",
+            'newline' => "\r\n"
+        ];
+
+        $this->load->library('email');
+        $post = $this->input->post();
+        $name = mb_convert_encoding($post['name'], "utf-8", "auto");
+        $email_sender = mb_convert_encoding($post['email'], "utf-8", "auto");
+        $message = mb_convert_encoding($post['message'], "utf-8", "auto");
+
+        $this->email->initialize($config);
+        $this->email->from($email_sender, $name);
+        $this->email->to('juliano.berselli@equipe.ezoom.com.br');
+        $this->email->subject('Duvida');
+        $this->email->message($message);
+
+        $this->email->send();
+    }
 }
